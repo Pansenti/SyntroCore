5fafbf9db8db11f9082d0106a66c1ac72f9e640f
Scott Ellis <scott@jumpnowtek.com>
2018-04-20 15:46:46 -0400

    Replace deprecated Q_WS_WIN with Q_OS_WIN

--- SyntroStore/StoreStream.cpp
+++ SyntroStore/StoreStream.cpp
@@ -307,7 +307,7 @@ bool StoreStream::checkFolderPermissions()
 	if (!info.exists() || !info.isDir())
 		return false;
 
-#ifdef Q_WS_WIN
+#ifdef Q_OS_WIN
 	return true;
 #else
 	return info.isWritable();
